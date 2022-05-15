5cda5809ba59766b4a57c85367c11731f6079272
Rafał Jasiński <259384@student.pwr.edu.pl>
2022-05-15 17:05:11 +0000

    Update parser.py

--- PROJECT_2/parser.py
+++ PROJECT_2/parser.py
@@ -3,4 +3,5 @@ import pandas as pd
 df = pd.read_csv('dane.csv', sep=',')
 df.dropna(inplace=True)
 df.drop('Unnamed: 0', inplace=True, axis=1)
+df.sort_values(["rating"],axis=0, ascending=[True], inplace=True)
 df.to_csv('poo.csv')
